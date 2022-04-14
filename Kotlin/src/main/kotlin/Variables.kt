5755def238969c2b73d3a767801bef24fa7593ee
Francisco JPP <francisco.jpp@hotmail.com>
2022-04-13 23:36:24 -0500

    Kotlin

---
+++ Kotlin/src/main/kotlin/Variables.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    //Constantes
+    val PI = 3.1416
+    val fechaDeNacimiento:String = "23-05-2001"
+    //Variables
+    var diaDeLaSemana = "Viernes"
+    var estadoDeAnimo:String = "Contento"
+    diaDeLaSemana = "Lunes"
+
+
+    println("Soy la variable de la semana con valor ${diaDeLaSemana}")
+}
