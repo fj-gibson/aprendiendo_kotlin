d8da8b5fb83b64fadab35c194a94000f1ba1ec02
Francisco JPP <francisco.jpp@hotmail.com>
2022-04-20 23:43:31 -0500

    Variables

---
+++ kotlin_2022/src/main/kotlin/String.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var caracter:Char = 'A' //Byte o 8 bits
+    println(caracter)
+    //ASCII
+    val caracterInt = 64
+    println(caracterInt.toChar())
+    println(caracter.toInt())
+    var caracterNumero = 'Z'
+    println(caracterNumero.digitToIntOrNull())
+}
